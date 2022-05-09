610f437c0ba34d25d495532223667b7f664b5d34
xiashuang2020 <xiashuang2020@gmail.com>
2022-05-09 19:00:52 +0800

    289

--- 17.py
+++ 17.py
@@ -1,2 +1,24 @@
+from typing import *
+
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        q = []
+        ret = []
+        m = {'2': "abc", '3': "def", '4': "ghi", '5': "jkl", '6': "mno", '7': "pqrs", '8': "tuv", '9': "wxyz"}
+
+        if len(digits) == 0:
+            return ret
+        q.append("")
+
+        while len(q) != 0:
+            s = q.pop(0)
+            l = len(s)
+            for x in m[digits[l]]:
+                if l == len(digits) - 1:
+                    ret.append(s + x)
+                else:
+                    q.append(s + x)
+        return ret
+
+solu = Solution()
+print(solu.letterCombinations("234"))
--- 289.c
+++ 289.c
@@ -2,8 +2,32 @@
 // Created by pyxisha on 2022/5/9.
 //
 
-
-
 void gameOfLife(int** board, int boardSize, int* boardColSize){
+    for (int i = 0; i < boardSize; ++i) {
+        for (int j = 0; j < boardColSize[0]; ++j) {
+            int surroundings[8][2] = {
+                    {i - 1, j - 1},
+                    {i - 1, j},
+                    {i - 1, j + 1},
+                    {i, j - 1},
+                    {i, j + 1},
+                    {i + 1, j - 1},
+                    {i + 1, j},
+                    {i + 1, j + 1}
+            };
+            int live_sib_cnt = 0;
 
+            for (int k = 0; k < 8; ++k) {
+                int a = surroundings[k][0];
+                int b = surroundings[k][1];
+                if (0 <= a && a < boardSize && 0 <= b && b < boardColSize[0]) {
+                    if (board[a][b] & 0x1) {
+                        live_sib_cnt++;
+                    }
+                }
+            }
+
+
+        }
+    }
 }
